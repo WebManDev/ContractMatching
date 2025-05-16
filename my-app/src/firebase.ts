6602fbc1dc0bfdf1ba5ef9ca0cdc8ff3882133b9
WebManDev <andyhluu23@gmail.com>
2025-05-17 00:09:36 +0100

    g

--- my-app/src/firebase.ts
+++ my-app/src/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_ID"
+  apiKey: "AIzaSyDKzwGwWKOQ8vUdzZuflIMieJYKVivLK-s",
+  authDomain: "contractmatch-ef2c5.firebaseapp.com",
+  projectId: "contractmatch-ef2c5",
+  storageBucket: "contractmatch-ef2c5.firebasestorage.app",
+  messagingSenderId: "970578936002",
+  appId: "1:970578936002:web:a3eed296badf5b28f1bb5b",
+  measurementId: "G-84YB3L31NC"
 };
 
 const app = initializeApp(firebaseConfig);
